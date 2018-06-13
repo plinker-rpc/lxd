b713a2093cb19c61265fcf06753479a43c94c4d7
lcherone <plinker@cherone.co.uk>
2018-06-13 03:44:46 +0000

    [since: v0.0.27]  13-Jun-2018 - added files

--- src/Containers/Files.php
+++ src/Containers/Files.php
@@ -37,10 +37,28 @@ class Files extends \Plinker\Lxd\Lib\Base
     /**
      *
      */
-     /*
-    public function list($remote = 'local', $container = '', $mutator = null)
+    public function list($remote = 'local', $container = '', $path = '', $mutator = null)
     {
-        return $this->query($remote.':'.$this->endpoint.'/'.$container.'/snapshots', 'GET', [], $mutator);
+        return $this->query($remote.':'.$this->endpoint.'/'.$container.'/files?path='.$path, 'GET', [], $mutator);
     }
-    */
+    
+    /**
+     *
+     */
+    public function push($remote = 'local', $container = '', $source = '', $path = '', $mutator = null)
+    {
+        $tmpname = tempnam("/tmp", "cert");
+        
+        file_put_contents($tmpname, $source);
+            
+        return $this->local('lxc file push -p --mode=644 --uid=0 --gid=0 '.$tmpname.' '.$remote.':'.$container.$path);
+    }
+    
+    /**
+     *
+     */
+    public function remove($remote = 'local', $container = '', $path = '', $mutator = null)
+    {
+        return $this->query($remote.':'.$this->endpoint.'/'.$container.'/files?path='.$path, 'DELETE', [], $mutator);
+    }
 }
